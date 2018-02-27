456f71ff94029d9671447f7cb5f09f49ca37dfbf
saintedlama <christoph.walcher@gmail.com>
2018-02-27 12:50:32 +0100

    feat: upgrade to mongo db native version 3

--- index.js
+++ index.js
@@ -1,26 +1,52 @@
-const mongojs = require('mongojs');
+const MongoClient = require('mongodb').MongoClient;
+const urlParser = require('mongodb/lib/url_parser');
+
 const async = require('async');
 
 const debug = require('debug')('drop-mongodb-collections');
 
 module.exports = function cleanAll(connectionString) {
-  const db = mongojs(connectionString, []);
-
   return function(done) {
-    debug('Starting to remove all entries from collections');
-
-    db.getCollectionNames((err, cols) => {
+    connect(connectionString, (err, { client, db }) => {
       if (err) { return done(err); }
 
-      cols = cols.filter(col => col.indexOf('system.') != 0);
+      db.listCollections().toArray((err, collections) => {
+        if (err) { return done(err); }
 
-      async.each(cols, (col, next) => {
-        db.collection(col).drop(next);
-      }, (err) => {
-        debug('Finished dropping collections with error result: %s', err || 'none');
+        async.each(collections, (collection, next) => db.dropCollection(collection.name, next), (err) => {
+          if (err) {
+            debug('Could not drop all collections due to error %s', err);
 
-        done(err);
+            return done(err);
+          }
+
+          debug('Finished dropping collections');
+  
+          client.close(true, (err) => {
+            if (err) { 
+              debug('Error while closing mongodb client %s', err); 
+            } else {
+              debug('Closed mongodb connection');
+            }
+
+            done();
+          });
+        });
       });
     });
   };
 };
+
+function connect(connectionString, next) {
+  urlParser(connectionString, (err, parsedUrl) => {
+    if (err) { return next(err); }
+
+    MongoClient.connect(connectionString, function(err, client) {
+      if (err) { return next(err); }
+      
+      const db = client.db(parsedUrl.dbName);
+    
+      next(null, { db, client });
+    });
+  });
+}
--- test/drop-mongodb-collections.js
+++ test/drop-mongodb-collections.js
@@ -1,42 +1,69 @@
 const expect = require('chai').expect;
-const mongojs = require('mongojs');
+const MongoClient = require('mongodb').MongoClient;
 
 const dropMongodbCollections = require('../');
 const connectionString = 'mongodb://localhost/drop-mongodb-collections-tests';
 
 describe('drop-mongodb-collections', function() {
+  let client;
+
+  beforeEach((next) => {
+    MongoClient.connect(connectionString, function(err, c) {
+      if (err) { return next(err); }
+      
+      client = c;
+    
+      next(null);
+    });
+  });
+
+  afterEach((next) => client.close(next));
+
   it('should drop all collections', function(done) {
-    const db = mongojs(connectionString);
-    db.items.insert({ name: 'John Doe'}, (err) => {
-      expect(err).to.not.exist;
+    const db = client.db('drop-mongodb-collections-tests');
+    const items = db.collection('items');
+
+    items.insert({ foo: 'bar' }, (err) => {
+      if (err) { return done(err); }
 
       const dropTestDb = dropMongodbCollections(connectionString);
 
       dropTestDb((err) => {
-        expect(err).to.not.exist;
+        if (err) { return done(err); }
 
         // And now verify that we have no more collections in the db
-        db.getCollectionNames((err, cols) => {
-          expect(err).to.not.exist;
-          expect(cols.length).to.equal(1); // 'system.indexes'
+        db.listCollections()
+          .toArray((err, cols) => {
+            if (err) { return done(err); }
 
-          done();
+            expect(cols.length).to.be.lte(1); // 'system.indexes'
+
+            done();
         });
       });
     });
   });
 
-  describe('mocha-before', function() {
-    before(dropMongodbCollections(connectionString));
+  describe('mocha-before-each', function() {
+    beforeEach((next) => {
+      const db = client.db('drop-mongodb-collections-tests');
+      const items = db.collection('items');
+
+      items.insert([{ foo: 'bar' }], next);
+    });
+
+    beforeEach(dropMongodbCollections(connectionString));
 
     it('Should drop all connections before tests', function(done) {
-      const db = mongojs(connectionString);
+      const db = client.db('drop-mongodb-collections-tests');
 
-      db.getCollectionNames((err, cols) => {
-        expect(err).to.not.exist;
-        expect(cols.length).to.equal(1); // 'system.indexes'
+      db.listCollections()
+        .toArray((err, cols) => {
+          if (err) { return done(err); }
 
-        done();
+          expect(cols.length).to.be.lte(1); // 'system.indexes'
+
+          done();
       });
     });
   })
